0d0622967b1ac442da3d8aa612407af43780f32a
kwon5700 <kwon777071@gmail.com>
2024-10-27 20:30:28 +0900

    Feat : Button 컴포넌트 개발

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../style/button.css'
+
+const Button = ({content}) => {
+
+    return (
+        <button>
+            {content}
+        </button>
+    )
+}
+
+export default Button
