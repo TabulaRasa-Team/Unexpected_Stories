86ef15e742fb37f5b1d1c179e44eb619af5db25e
winshine0326 <shane10340@gmail.com>
2024-11-04 00:31:05 +0900

    feat: 감정표현 버튼 추가

--- src/components/ReadStory.jsx
+++ src/components/ReadStory.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../style/ReadStory.css'
 import ReadStoryPost from './ReadStoryPost'
+import happy_emoji from '../images/happy_emoji.svg'
+import kiss_emoji from '../images/kiss_emoji.svg'
+import tear_emoji from '../images/tear_emoji.svg'
 
 const ReadStory = () => {
     console.log("hello")
     return (
         <div className='bg-container'>
             <ReadStoryPost/>
+            <div className='emojis'>
+                <img src={happy_emoji}/>
+                <img src={tear_emoji}/>
+                <img src={kiss_emoji}/>
+            </div>
         </div>
     )
 }
