5e94e2ee601f63f66e3aaf06c69cd83046d236b2
kwon5700 <kwon777071@gmail.com>
2024-11-03 14:12:54 +0900

    Chore : <></> 제거

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,27 +7,25 @@ import view from '../images/Eye_light.svg'
 const Post = (content, key) => {
     let data = content.content
     return (
-        <>
-            <li>
-                <div>
-                    <h3 className='date'>{data.date}</h3>
-                    <div className='article'>
-                        <div className='articleFront'>
-                            <h4 className='articleTitle'>{data.title}</h4>
-                            <span className='content'>{data.content}</span>
+        <li>
+            <div>
+                <h3 className='date'>{data.date}</h3>
+                <div className='article'>
+                    <div className='articleFront'>
+                        <h4 className='articleTitle'>{data.title}</h4>
+                        <span className='content'>{data.content}</span>
+                    </div>
+                    <div className='articleBack'>
+                        <div className='articleItem'>
+                            <img src={favorite} alt="좋아요" /><div> {data.like}</div>
                         </div>
-                        <div className='articleBack'>
-                            <div className='articleItem'>
-                                <img src={favorite} alt="좋아요" /><div> {data.like}</div>
-                            </div>
-                            <div className='articleItem'>
-                                <img src={view} alt="조회수" /><div> {data.view}</div>
-                            </div>
+                        <div className='articleItem'>
+                            <img src={view} alt="조회수" /><div> {data.view}</div>
                         </div>
                     </div>
                 </div>
-            </li>
-        </>
+            </div>
+        </li>
     )
 }
 
