4a1c1a80f6f72865b0460398955d1eadaa938963
winshine0326 <shane10340@gmail.com>
2024-11-04 00:43:50 +0900

    feat: 버튼 컴포넌트 margin 프롭스 추가 및 읽기 페이지 편지 닫기 버튼 추가

--- src/components/ReadStory.jsx
+++ src/components/ReadStory.jsx
@@ -4,6 +4,7 @@ import ReadStoryPost from './ReadStoryPost'
 import happy_emoji from '../images/happy_emoji.svg'
 import kiss_emoji from '../images/kiss_emoji.svg'
 import tear_emoji from '../images/tear_emoji.svg'
+import Button from './Button'
 
 const ReadStory = () => {
     console.log("hello")
@@ -15,6 +16,7 @@ const ReadStory = () => {
                 <img src={tear_emoji}/>
                 <img src={kiss_emoji}/>
             </div>
+            <Button content={"편지 닫기"} margin="5% auto"/>
         </div>
     )
 }
