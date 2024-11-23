6f21675f221b4b0884679493757cab1a5cca7f03
kwon5700 <kwon777071@gmail.com>
2024-11-24 03:08:18 +0900

    Fix : get 변수명 변경

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -119,13 +119,13 @@ function MyPage() {
                 {reversePosts.map((item) => (
                     <Post 
                         data={item}
-                        key={item.text_id-1}
+                        key={item.textId-1}
                         onClick={() => {
-                            setSelectedId(item.text_id)
+                            setSelectedId(item.textId)
                             setSelected(true)
                         }}
                         isSelected={selected}
-                        selectedId={selectedId == item.text_id ? selectedId : false}
+                        selectedId={selectedId == item.textId ? selectedId : false}
                     />
                 ))}
             </ul>
